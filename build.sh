0698c1a8ef18b04b11b4ce72e92c35f54a962bd3
pdp <pdp@gnucitizen.org>
2015-06-22 21:13:07 +0100

    Package rearrangement for easier compilation.

--- build.sh
+++ build.sh
@@ -12,8 +12,8 @@ CSD=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 docker run \
 	--rm \
-	-v "${CSD}/v1:/go/src/v1"\
-	-v "${CSD}/main:/src" \
+	-v "${CSD}/src/v1:/go/src/v1"\
+	-v "${CSD}/bin:/src" \
 	-v "/var/run/docker.sock:/var/run/docker.sock" \
 	"centurylink/golang-builder" \
 	"websecurify/go-user-microservice"
@@ -22,6 +22,6 @@ docker run \
 # ---
 # ---
 
-rm "${CSD}/main/go-user-microservice"
+rm "${CSD}/bin/go-user-microservice"
 
 # ---
